6f189ba52649ca0332d252bbf16b1aeba8d54377
Luna Fernandez <Luna.Fernandez@anniecannonss-MacBook-Pro.local>
2024-06-12 12:21:51 -0600

    added contact page

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,17 @@ function showHamburger() {
   nav.classList.toggle("nav-active")
 }
 
+const form = document.getElementById("form");
+let userArray = [];
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+	const data = new FormData(event.target);
+	const dataObject = Object.fromEntries(data.entries());
+
+	userArray.push(dataObject);
+	console.log(userArray);
+	form.reset();
+}
